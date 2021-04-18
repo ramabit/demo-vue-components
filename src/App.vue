f599f7a9decb70811b2f6664cfe8e5864542a9a7
Fernando Ramirez <fernando.ramirez@cybercon.de>
2021-04-18 12:41:12 +0200

    adding dropdown to select the page using the custom select component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,39 @@
 <template>
   <div id="app">
 
-    <span>
+    <!-- Dropdown component with options-->
+    <custom-select
+        @valueChanged="redirectToPage"
+        :options="menuOptions"
+        inline-template
+    >
+      <div class="c-custom-select">
+        <div class="selector" @click="toggleOptionVisible()">
+          <div class="label">
+            <span>{{ value }}</span>
+          </div>
+          <div class="arrow" :class="{ 'expanded' : optionsVisible }"></div>
+          <div v-if="optionsVisible">
+            <ul>
+              <li
+                  :class="{ 'current': item === value }"
+                  v-for="(item, index) in options"
+                  :key="index"
+                  @click="select(item)"
+              >{{ item }}</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </custom-select>
+
+    <!-- Select amount of tabs to show -->
+    <div class="u-margin--top-2bu">
       Insert the amount of desired user's tabs to load:
        <input v-model="tabs" placeholder="Insert amount of tabs">
-    </span>
+    </div>
 
+    <!-- Users tabs -->
     <users-tabs class="u-margin--top-1bu" :amount="parseInt(tabs)"></users-tabs>
 
 <!-- Test insert more than 1 tabs component at the time -->
@@ -22,7 +50,19 @@ export default {
   data() {
     return {
       tabs: 3,
+      menuOptions: [
+          'Main page',
+          'Account',
+          'Settings',
+          'Logout'
+      ]
     }
   },
+  methods: {
+    redirectToPage(option) {
+      // TODO implement redirect to page
+      console.log('redirect to: ', option)
+    }
+  }
 }
 </script>
